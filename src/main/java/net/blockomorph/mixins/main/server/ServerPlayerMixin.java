eb47536ea91d32b9a6d44202e366d0a493832011
151chit <151gamemode1chit@gmail.com>
2025-07-01 22:38:59 +0300

    MorphUtils.java fix

--- src/main/java/net/blockomorph/mixins/main/server/ServerPlayerMixin.java
+++ src/main/java/net/blockomorph/mixins/main/server/ServerPlayerMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemCooldowns;
@@ -85,5 +86,10 @@ public abstract class ServerPlayerMixin extends Player {
         } else {
             pl.loadBlockData(oldPl.saveBlockData(false), null);
         }
+    }
+
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    public void hurt(DamageSource damageSource, float f, CallbackInfoReturnable<Boolean> cir) {
+        if (MorphUtils.onPlayerAttacked(damageSource, this)) cir.setReturnValue(false);
     }
 }
