0570d8b743bf847e85d6c7fbc2eca419a58fe127
151chit <151gamemode1chit@gmail.com>
2025-08-14 20:36:39 +0300

    sync patch

--- src/main/java/net/blockomorph/mixins/main/PlayerMixin.java
+++ src/main/java/net/blockomorph/mixins/main/PlayerMixin.java
@@ -1,6 +1,5 @@
 package net.blockomorph.mixins.main;
 
-import net.blockomorph.Blockomorph;
 import net.blockomorph.network.ClientBoundApplyBlockMorphPacket;
 import net.blockomorph.network.ClientBoundMorphUpdatePacket;
 import net.blockomorph.network.ClientBoundServerBlockEntityTagPacket;
@@ -15,9 +14,6 @@ import net.minecraft.nbt.NbtUtils;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundBlockEventPacket;
 import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -54,8 +50,7 @@ import java.util.function.Predicate;
 public abstract class PlayerMixin extends LivingEntity implements PlayerAccessor {
 	@Shadow protected abstract boolean canPlayerFitWithinBlocksAndEntitiesWhen(Pose p_294172_);
 
-	private static final EntityDataAccessor<Integer> TNT_PROGRESS = SynchedEntityData.defineId(Player.class, EntityDataSerializers.INT);
-	private final TntHandler TNT_HANDLER = new TntHandler(this, this.entityData, TNT_PROGRESS);
+	private final TntHandler TNT_HANDLER = new TntHandler(this);
 	private final HitBoxCalculator HITBOX_HANDLER = new HitBoxCalculator(this);
 	private final ConcurrentHashMap<InPlayerBlockPos, BlockInPlayer2> blocksData = new ConcurrentHashMap<>();
 	private final List<InPlayerBlockPos> updates = new CopyOnWriteArrayList<>();
@@ -139,11 +134,6 @@ public abstract class PlayerMixin extends LivingEntity implements PlayerAccessor
 
 	public boolean isBreaking() {
 		return this.breakingMode;
-	}
-
-	@Inject(method = "defineSynchedData", at = @At("TAIL"), cancellable = true)
-	protected void defineSynchedData(SynchedEntityData.Builder entityData, CallbackInfo ci) {
-		entityData.define(TNT_PROGRESS, -1);
 	}
 
 	@Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
