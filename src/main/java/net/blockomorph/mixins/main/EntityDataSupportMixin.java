9f96b7e80506a81587b99f6d47878e95f912412e
151chit <151gamemode1chit@gmail.com>
2025-08-14 19:50:21 +0300

    sync patch

---
+++ src/main/java/net/blockomorph/mixins/main/EntityDataSupportMixin.java
@@ -0,0 +1,49 @@
+package net.blockomorph.mixins.main;
+
+import net.blockomorph.network.ClientBoundEntityDataSyncPacket;
+import net.blockomorph.utils.dataSyncher.SyncedEntity;
+import net.blockomorph.utils.dataSyncher.AutoSyncedEntityData;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+@Mixin(Entity.class)
+public abstract class EntityDataSupportMixin implements SyncedEntity {
+
+	@Shadow private int id;
+	private final List<AutoSyncedEntityData<?>> SYNCERS = new ArrayList<>();
+	private boolean dirty;
+
+	public void registerDataSyncer(AutoSyncedEntityData<?> data) {
+		if (this.getDataById(data.getId()) != null) throw new IllegalArgumentException("Data with id: " + this.id + " already registered!");
+		SYNCERS.add(data);
+	}
+
+	public void setDirty() {
+		this.dirty = true;
+	}
+
+	public void checkOrSendImmediate(Consumer<ClientBoundEntityDataSyncPacket> doing, boolean force) {
+		if (!this.dirty && !force) return;
+		for (AutoSyncedEntityData<?> data : SYNCERS) {
+			if (data.isDirty() || force) {
+				doing.accept(new ClientBoundEntityDataSyncPacket(this, data));
+			}
+		}
+		this.dirty = false;
+	}
+
+	@Nullable
+	public AutoSyncedEntityData<?> getDataById(ResourceLocation id) {
+		for (AutoSyncedEntityData<?> data : SYNCERS) {
+			if (data.getId().equals(id)) return data;
+		}
+		return null;
+	}
+}
--- src/main/java/net/blockomorph/mixins/main/server/ServerEntityMixin.java
+++ src/main/java/net/blockomorph/mixins/main/server/ServerEntityMixin.java
@@ -1,13 +1,16 @@
 package net.blockomorph.mixins.main.server;
 
+import net.blockomorph.network.BlockMorphPacket;
 import net.blockomorph.network.ClientBoundBlockPosBoundPacket;
 import net.blockomorph.network.ClientBoundMorphUpdatePacket;
 import net.blockomorph.utils.coords.BlockPosBounds;
 import net.blockomorph.utils.MorphUtils;
 import net.blockomorph.utils.PlayerAccessor;
 import net.blockomorph.utils.coords.PlayerMorphedSection;
+import net.blockomorph.utils.dataSyncher.SyncedEntity;
 import net.minecraft.network.protocol.game.ClientboundSetPassengersPacket;
 import net.minecraft.server.level.ServerEntity;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ChunkPos;
@@ -23,12 +26,19 @@ public class ServerEntityMixin {
 
 	@Shadow @Final private Entity entity;
 
-	@Inject(method = "sendChanges", at = @At(value = "INVOKE", target = "Ljava/util/function/BiConsumer;accept(Ljava/lang/Object;Ljava/lang/Object;)V", shift = At.Shift.AFTER))
+    @Shadow @Final private ServerLevel level;
+
+    @Inject(method = "sendChanges", at = @At(value = "INVOKE", target = "Ljava/util/function/BiConsumer;accept(Ljava/lang/Object;Ljava/lang/Object;)V", shift = At.Shift.AFTER))
 	private void fixAsync(CallbackInfo ci) {
 		if (this.entity instanceof ServerPlayer pl) {
 			pl.connection.send(new ClientboundSetPassengersPacket(pl));
 		}
 	}
+
+    @Inject(method = "sendChanges", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getPassengers()Ljava/util/List;"))
+    public void sync(CallbackInfo ci) {
+        SyncedEntity.of(this.entity).checkOrSendImmediate(this::sendCustomPacket, false);
+    }
 
 	@Inject(method = "addPairing", at = @At(value = "TAIL"))
 	public void start(ServerPlayer looker, CallbackInfo ci) {
@@ -39,6 +49,7 @@ public class ServerEntityMixin {
 			}
 			MorphUtils.sendPlayer(new ClientBoundMorphUpdatePacket(acc), looker);
 		}
+        SyncedEntity.of(this.entity).checkOrSendImmediate(this::sendCustomPacket, true);
 	}
 
 	@Inject(method = "removePairing", at = @At(value = "TAIL"))
@@ -49,5 +60,12 @@ public class ServerEntityMixin {
 				MorphUtils.sendPlayer(new ClientBoundBlockPosBoundPacket(pos, pl, true), looker);
 			}
 		}
-	}
+    }
+
+    private void sendCustomPacket(BlockMorphPacket packet) {
+        this.level.getChunkSource().chunkMap.getPlayers(this.entity.chunkPosition(), false).forEach(player -> {
+            MorphUtils.sendPlayer(packet, player);
+        });
+    }
+
 }
