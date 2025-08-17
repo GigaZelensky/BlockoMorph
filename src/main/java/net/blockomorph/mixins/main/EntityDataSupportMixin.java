561176bf4102969093feb49d0cb6d78786ecb722
151chit <151gamemode1chit@gmail.com>
2025-08-17 21:18:00 +0300

    sync crash patch

--- src/main/java/net/blockomorph/mixins/main/EntityDataSupportMixin.java
+++ src/main/java/net/blockomorph/mixins/main/EntityDataSupportMixin.java
@@ -6,7 +6,6 @@ import net.blockomorph.utils.dataSyncher.AutoSyncedEntityData;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -18,12 +17,12 @@ import java.util.function.Consumer;
 public abstract class EntityDataSupportMixin implements SyncedEntity {
 
 	@Shadow private int id;
-	private final List<AutoSyncedEntityData<?>> SYNCERS = new ArrayList<>();
+	private List<AutoSyncedEntityData<?>> SYNCERS;
 	private boolean dirty;
 
 	public void registerDataSyncer(AutoSyncedEntityData<?> data) {
 		if (this.getDataById(data.getId()) != null) throw new IllegalArgumentException("Data with id: " + this.id + " already registered!");
-		SYNCERS.add(data);
+		this.get().add(data);
 	}
 
 	public void setDirty() {
@@ -32,7 +31,7 @@ public abstract class EntityDataSupportMixin implements SyncedEntity {
 
 	public void checkOrSendImmediate(Consumer<ClientBoundEntityDataSyncPacket> doing, boolean force) {
 		if (!this.dirty && !force) return;
-		for (AutoSyncedEntityData<?> data : SYNCERS) {
+		for (AutoSyncedEntityData<?> data : this.get()) {
 			if (data.isDirty() || force) {
 				doing.accept(new ClientBoundEntityDataSyncPacket(this, data));
 			}
@@ -42,9 +41,16 @@ public abstract class EntityDataSupportMixin implements SyncedEntity {
 
 	@Nullable
 	public AutoSyncedEntityData<?> getDataById(ResourceLocation id) {
-		for (AutoSyncedEntityData<?> data : SYNCERS) {
+		for (AutoSyncedEntityData<?> data : this.get()) {
 			if (data.getId().equals(id)) return data;
 		}
 		return null;
+	}
+
+	private List<AutoSyncedEntityData<?>> get() {
+		if (SYNCERS == null) {
+			SYNCERS = new ArrayList<>();
+		}
+		return SYNCERS;
 	}
 }
